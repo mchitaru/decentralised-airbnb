fd98270a85879e934934e2152cb779267d00e907
corsair <marius.chitaru@gmail.com>
2022-07-22 09:33:35 +0300

    hardhat config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,7 +2,31 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  }, 
+  // defaultNetwork: "ganache",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    ganache: {
+      chainId: 1337,
+      url: "http://127.0.0.1:7545",
+      // accounts: ["2bb827c8456251a9a348bff75e46a32ef24e87c8e0c8e43f54f1cfaab828747b",
+      //             "a84f10062c2cea32b20f60591927f91ad1c0bdfb550108b4d47924ac74908dc4",
+      //             "4bdbb4eb97528b8907f2752bd908fa90b70a15cc569f44d0bfbaabbbb661f459"]
+    }
+  },
+  paths: {
+    artifacts: "../src/artifacts"
+  }
 };
 
 export default config;
